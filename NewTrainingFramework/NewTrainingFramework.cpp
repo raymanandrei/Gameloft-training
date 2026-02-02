19d329f23ed149d6f7e06daf38c50205b9e19fc5
Andrei <bejanandreialexandru@gmail.com>
2026-02-02 15:10:48 +0200

    Separated readNFG function to its own file

--- NewTrainingFramework/NewTrainingFramework.cpp
+++ NewTrainingFramework/NewTrainingFramework.cpp
@@ -4,8 +4,6 @@
 #include <string>
 #include <vector>
 #include <conio.h>
-#include <fstream>
-#include <sstream>
 #include "Vertex.h"
 #include "Shaders.h"
 #include "Globals.h"
@@ -15,6 +13,8 @@
 #include "SceneObject.h"
 
 #include "../Utilities/utilities.h" // if you use STL, please include this line AFTER all other include
+
+#include "ReadNFG.h"
 
 GLuint modelVboId;
 GLuint modelIboId;
@@ -34,102 +34,6 @@ Camera camera = Camera();
 
 ResourceManager* resourceManager = ResourceManager::GetInstance();
 SceneManager* sceneManager = SceneManager::GetInstance();
-
-void readNfgLine(std::string str, std::string field, std::vector<float>& numbers) {
-
-	int posIndex = str.find(field);
-
-	if (posIndex != -1) {
-
-		std::string numbersStr = str.substr(posIndex + field.length() + 1, str.find(';') - posIndex - field.length() - 1);
-		std::stringstream ss(numbersStr);
-		std::string token;
-
-		while (std::getline(ss, token, ','))
-			numbers.push_back(std::stod(token));
-	}
-}
-
-void readNfg(std::string filename, std::vector<Vertex>& vertices, std::vector<unsigned short>& indices) {
-	std::ifstream file(filename);
-	std::string str;
-	while (std::getline(file, str))
-	{
-		std::vector<float> numbers;
-		Vertex v;
-		readNfgLine(str, "pos:", numbers);
-
-		if (numbers.size() == 3) {
-
-			v.pos.x = numbers[0];
-			v.pos.y = numbers[1];
-			v.pos.z = numbers[2];
-		}
-
-		str = str.substr(str.find(";") + 1, str.length() - str.find(";") - 1);
-		numbers.clear();
-		readNfgLine(str, "norm:", numbers);
-
-		if (numbers.size() == 3) {
-			v.norm.x = numbers[0];
-			v.norm.y = numbers[1];
-			v.norm.z = numbers[2];
-		}
-
-		str = str.substr(str.find(";") + 1, str.length() - str.find(";") - 1);
-		numbers.clear();
-		readNfgLine(str, "binorm:", numbers);
-
-		if (numbers.size() == 3) {
-			v.binorm.x = numbers[0];
-			v.binorm.y = numbers[1];
-			v.binorm.z = numbers[2];
-		}
-
-		str = str.substr(str.find(";") + 1, str.length() - str.find(";") - 1);
-
-		numbers.clear();
-		readNfgLine(str, "tgt:", numbers);
-
-		if (numbers.size() == 3) {
-			v.tgt.x = numbers[0];
-			v.tgt.y = numbers[1];
-			v.tgt.z = numbers[2];
-		}
-
-		str = str.substr(str.find(";") + 1, str.length() - str.find(";") - 1);
-
-		numbers.clear();
-		readNfgLine(str, "uv:", numbers);
-
-		if (numbers.size() == 2) {
-			v.uv.x = numbers[0];
-			v.uv.y = numbers[1];
-			vertices.push_back(v);
-		}
-
-		int posIndex = str.find("NrIndices:");
-		if (posIndex != std::string::npos) {
-			std::string numberStr = str.substr(posIndex + 10);
-			int nrIndices = std::stoi(numberStr);
-			while (std::getline(file, str)) {
-
-				int posIndex = str.find(".");
-
-				std::string numbersStr = str.substr(posIndex + 1, str.find('.', posIndex + 1) - posIndex - 1);
-				std::stringstream ss(numbersStr);
-				std::string token;
-
-				while (std::getline(ss, token, ',')) {
-					indices.push_back(std::stoi(token));
-				}
-
-			}
-		}
-
-	}
-}
-
 
 int Init ( ESContext *esContext )
 {
